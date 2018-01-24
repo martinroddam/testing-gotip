61832392f4ff39e4bb6cfa0bc19d06698c983d3a
Martin Roddam <martin.roddam@gmail.com>
2018-01-24 11:12:15 +0000

    Doing a pull request

--- test-gotip.go
+++ test-gotip.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 
 	"github.com/martinroddam/go-tip"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/utilitywarehouse/go-operational/op"
 )
 
 type RadioButton struct {
@@ -27,6 +29,19 @@ func main() {
 	http.HandleFunc("/", DisplayRadioButtons)
 	http.HandleFunc("/selected", UserSelected)
 	http.HandleFunc("/fruitselected", UserFruitSelected)
+	dm := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "dummy_metric",
+		Help: "Dummy counter",
+	})
+	http.Handle("/__/", op.NewHandler(
+		op.NewStatus("My application", "application that does stuff").
+			AddOwner("team x", "#team-x").
+			SetRevision("7470d3dc24ce7876a9fc53ca7934401273a4017a").
+			AddChecker("db check", func(cr *op.CheckResponse) { cr.Healthy("dummy db connection check succeesed") }).
+			AddMetrics(dm).
+			ReadyUseHealthCheck(),
+	),
+	)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
